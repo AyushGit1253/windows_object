6531259da0ad33692a48690751a5832969e1cfad
AyushGit1253 <ayush1253khadatkar@gmail.com>
2023-07-14 20:58:42 +0530

    bookingapp

--- main.js
+++ main.js
@@ -5,17 +5,42 @@ form.addEventListener('submit',function(e){
 
     var name=document.getElementById('name').value
     var email=document.getElementById('email').value
+    var phone=document.getElementById('phone').value
+    var userList=document.getElementById('users')
 
     var user={
         name:name,
-        email:email
+        email:email,
+        phone:phone
     }
 
-    localStorage.setItem('user',JSON.stringify(user))
+    localStorage.setItem(user.email,JSON.stringify(user))
+
+    var msg=document.querySelector('.msg')
+    msg.textContent='data stored in local storage'
+    msg.style.color='red'
+
+   var li=document.createElement('li')
+    li.textContent=user.name+' '+user.email+' '+user.phone
+    userList.appendChild(li) 
+    
+    name.textContent=''
+    email=''
+    phone=''
+    //userList.innerHTML=userList.innerHTML+<li> user.email +user.name+user.phone</li>
+           //     OR
+    
+})
+   /*  localStorage.setItem('user',JSON.stringify(user))
     name=''
     email=''
 
     var msg=document.querySelector('.msg')
     msg.textContent='data stored in local storage'
-    msg.computedStyleMap.color='red'
-})
+    msg.style.color='red' */
+
+   /*  var newUser=localStorage.getItem('users')//get data from local if any
+    var users=newUser ? JSON.parse(newUser): [] *///if data then parse no then empty array return
+
+    //users.push(user)
+  
